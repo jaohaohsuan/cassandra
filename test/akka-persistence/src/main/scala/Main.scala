a7a6caa59243d083c12af1006f78d0e5e95f01d8
henry <jaohaohsuan@gmail.com>
2017-03-21 15:20:51 +0800

    apply Journal schema

--- test/akka-persistence/src/main/scala/Main.scala
+++ test/akka-persistence/src/main/scala/Main.scala
@@ -1,7 +1,18 @@
 import akka.actor.ActorSystem
+import akka.persistence.cassandra.query.scaladsl.CassandraReadJournal
+import akka.persistence.query.PersistenceQuery
+
+import scala.io.StdIn
 
 object Main {
   def main(args: Array[String]): Unit = {
-    println("hello scala")
+
+    implicit val system = ActorSystem("system")
+
+    println("Connect to cassandra Journal")
+
+    val readJournal = PersistenceQuery(system).readJournalFor[CassandraReadJournal](CassandraReadJournal.Identifier)
+
+    StdIn.readLine()
   }
 }
