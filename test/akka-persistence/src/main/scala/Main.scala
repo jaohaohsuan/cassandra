256793b2a23efd7a2946263334e1ee98849a9c1d
henry <jaohaohsuan@gmail.com>
2017-03-22 11:40:46 +0800

    try to fix connection

--- test/akka-persistence/src/main/scala/Main.scala
+++ test/akka-persistence/src/main/scala/Main.scala
@@ -1,7 +1,11 @@
 import akka.actor.ActorSystem
 import akka.persistence.cassandra.query.scaladsl.CassandraReadJournal
 import akka.persistence.query.PersistenceQuery
-
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
+import akka.pattern.after
+import akka.actor.Scheduler
 import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
 import scala.util.{Failure, Success}
@@ -13,11 +17,17 @@ object Main {
 
     implicit val exec = system.dispatcher
 
+    implicit val s = system.scheduler
+
     println("Connect to cassandra Journal")
 
     val readJournal: CassandraReadJournal = PersistenceQuery(system).readJournalFor[CassandraReadJournal](CassandraReadJournal.Identifier)
 
-    readJournal.session.underlying().onComplete {
+    def retry[T](f: => Future[T], retries: Int, defaultDelay: FiniteDuration )(implicit ec: ExecutionContext, s: Scheduler): Future[T] = {
+      f recoverWith { case _ if retries > 0 => after(defaultDelay, s)(retry(f, retries - 1 , defaultDelay)) }
+    }
+
+    retry(readJournal.session.underlying, 10, 3 seconds).onComplete {
       case Failure(_) =>
         system.scheduler.scheduleOnce(5 seconds) {
           Await.result(system.terminate(), 3 seconds)
